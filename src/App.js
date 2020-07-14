c48ae8df556903bd30c9232aa2ec16ca9d9222ca
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-14 16:57:35 -0700

    projects and project images update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Header } from './components/Header';
+import Header from './components/Header';
 import Bio from './components/Bio';
+import Projects from './components/Projects';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <Bio />
+      <Projects />
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from 'react';
+import password_generator from '../images/password_generator.png';
+import workday_scheduler from '../images/workday_scheduler.png';
+import gamrblack300px from '../images/gamrblack300px.png';
 
 export const Projects = () => {
-    return (
-        <div>
-            <p>Projects</p>
+  return (
+    <div>
+      <h2 className='text-center'>PROJECTS</h2>
+      <div className='row'>
+        <div className='col'>
+          <div
+            className='card align-items-center p-3 border-0 text-left'
+            style={{ width: '18rem' }}
+          >
+            <a
+              href='https://davidcastrodesign.github.io/passwordgenerator/'
+              target='blank'
+            >
+              <img
+                src={password_generator}
+                className='card-img-top'
+                alt='Password Generator'
+              />
+            </a>
+            <div className='card-body'>
+              <a href='https://github.com/davidcastrodesign/passwordgenerator'>
+                GitHub Link
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+
+        <div className='col'>
+          <div
+            className='card align-items-center p-3 border-0 text-left'
+            style={{ width: '18rem' }}
+          >
+            <a
+              href='https://davidcastrodesign.github.io/Work-Day-Scheduler/'
+              target='blank'
+            >
+              <img
+                src={workday_scheduler}
+                className='card-img-top'
+                alt='Workday Scheduler'
+              />
+            </a>
+            <div className='card-body'>
+              <a href='https://github.com/davidcastrodesign/Work-Day-Scheduler'>
+                GitHub Link
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='col'>
+          <div
+            className='card align-items-center p-3 border-0 text-left'
+            style={{ width: '18rem' }}
+          >
+            <a href='https://jhnpn.github.io/Project-1/' target='blank'>
+              <img src={gamrblack300px} className='card-img-top' alt='Gamer' />
+            </a>
+            <div className='card-body'>
+              <a href='https://github.com/jhnpn/Project-1/tree/David-branch'>
+                GitHub Link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
